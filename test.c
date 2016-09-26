086066bc95f93ca11ae3d77a9f864f3dc05121dc
mithrilwoodrat <mithtilwoodrat@gmail.com>
2016-09-26 11:32:17 +0800

    add heap manage funcs

--- test.c
+++ test.c
@@ -15,10 +15,21 @@ void test_iota()
     puts(len_str);
 }
 
+void test_malloc()
+{
+    int *p_int = (int *) malloc(sizeof(int));
+    *p_int = 10;
+    char len_str[10];
+    itoa(*p_int, len_str);
+    puts(len_str);
+    free(p_int);
+}
+
 int main(int argc,char * argv[])
 {
     test_puts();
     test_iota();
+    test_malloc();
     getchar();
     return 42;
 }
--- toy_crt.h
+++ toy_crt.h
@@ -4,6 +4,10 @@
 /*
  * from 程序员的自我修养 minicrt
  */
+#ifndef NULL
+#define NULL (0)
+#endif
+
 
 #define X86_64 1
 typedef unsigned long size_t;
@@ -22,11 +26,14 @@ int crt_heap_init(void);
 int read(int fd, void *buffer, size_t size);
 int write(int fd, const void *buffer, size_t size);
 void puts(const char *str);
+void putchar(int c);
 int getchar(void);
 
 void itoa(int n, char s[]);
 size_t strlen(const char *str);
 
-#define NULL 0
+//from stdlib.h
+void *malloc(size_t size);
+void free(void *ptr);
 
 #endif
